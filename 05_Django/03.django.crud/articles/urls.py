6d30480f4271847349b8111cda1a1b4947a9cbc6
Student <student@M150127>
2019-11-04 17:36:18 +0900

    DB 1:n관계 적용

--- 05_Django/03.django.crud/articles/urls.py
+++ 05_Django/03.django.crud/articles/urls.py
@@ -4,10 +4,11 @@ from . import views
 app_name = 'articles'
 urlpatterns = [
     path('<int:article_pk>/update/', views.update, name='update'),
-    path('<int:article_pk>/edit/', views.edit, name='edit'),
-    path('<int:article_pk>/delete/', views.delete, name='delete'),
+    # path('<int:article_pk>/edit/', views.edit, name='edit'),
+    path('<int:article_pk>/delete/', views.delete, name='delete'),  # delete logic
+
     path('<int:article_pk>/', views.detail, name='detail'),
-    path('create/', views.create),
-    path('new/', views.new),
+    path('create/', views.create),  # GET(new) / POST(create)
+    # path('new/', views.new),
     path('', views.index, name='index'),
 ]
--- 05_Django/03.django.crud/articles/views.py
+++ 05_Django/03.django.crud/articles/views.py
@@ -8,18 +8,32 @@ def index(request):
     return render(request, 'articles/index.html', context)
 
 # 사용자에게 게시글 작성 폼을 보여주는 함수
-def new(request):
-    return render(request, 'articles/new.html')
+# def new(request):
+#     return render(request, 'articles/new.html')
 
 # 사용자로부터 데이터를 받아서 DB에 저장하는 함수
-def create(request):
-    title = request.POST.get('title')
-    content = request.POST.get('content')
+# def create(request):
+#     title = request.POST.get('title')
+#     content = request.POST.get('content')
 
-    article = Article(title=title, content=content)
-    article.save()
-    # return render(request, 'articles/create.html')
-    return redirect('/articles/')
+#     article = Article(title=title, content=content)
+#     article.save()
+#     # return render(request, 'articles/create.html')
+#     return redirect('/articles/')
+
+def create(request):
+    # POST 요청일 경우 -> 게시글 생성 로직 수행
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+
+        article = Article(title=title, content=content)
+        article.save()
+        return redirect('articles:detail', article.pk)
+    # GET 요청일 경우 -> 사용자에게 폼 보여주기
+    else:
+        return render(request, 'articles/create.html')
+
 
 # 게시글 상세정보를 가져오는 함수
 def detail(request, article_pk):
@@ -27,25 +41,49 @@ def detail(request, article_pk):
     context = {'article': article}
     return render(request, 'articles/detail.html', context)
 
+# def detail(request, article_pk):
+#     if request.method == 'POST':
+#         article = Article.objects.get(pk=article_pk)
+#         context = {'article': article}
+#         return render(request, 'articles/detail.html', context)
+#     else:
+        
+
 def delete(request, article_pk):
     article = Article.objects.get(pk=article_pk)
     article.delete()
     return redirect('/articles/')
 
-# 사용자에게 수정 폼 던져줌
-def edit(request, article_pk):
-    article = Article.objects.get(pk=article_pk)
-    context = {'article': article}
-    return render(request, 'articles/edit.html', context)
+# # 사용자에게 수정 폼 던져줌
+# def edit(request, article_pk):
+#     article = Article.objects.get(pk=article_pk)
+#     context = {'article': article}
+#     return render(request, 'articles/edit.html', context)
+
+# # 데이터 받아서 DB에 반영
+# def update(request, article_pk):
+#     article = Article.objects.get(pk=article_pk)
+#     article.title = request.POST.get('title')
+#     article.content = request.POST.get('content')
+#     article.save()
+#     # return redirect(f'/articles/{article.pk}/')       # 1. 하드코딩
+#     return redirect('articles:detail', article.pk) # 2. URL Namespace
+
 
 # 데이터 받아서 DB에 반영
 def update(request, article_pk):
     article = Article.objects.get(pk=article_pk)
-    article.title = request.POST.get('title')
-    article.content = request.POST.get('content')
-    article.save()
-    # return redirect(f'/articles/{article.pk}/')       # 1. 하드코딩
-    return redirect('articles:detail.html', article.pk) # 2. URL Namespace
+
+    # POST 요청 : DB에 수정사항 반영
+    if request.method == "POST":
+        article.title = request.POST.get('title')
+        article.content = request.POST.get('content')
+        article.save()
+        return redirect('articles:detail', article.pk)
+    # GET 요청 : 사용자에게 수정 Form 전달
+    else:
+        context = {'article': article}
+        return render(request, 'articles/update.html', context)
 
     # .html 파일 내에서 '{% url %} 템플릿 태그' 사용햇을 때(헷갈림 주의!!!!)
     # <a href="{% url articles:detail article.pk %}">
