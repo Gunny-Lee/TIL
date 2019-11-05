74cf1dc7d2ba457790da77193e178fa264280c93
Student <student@M150127>
2019-11-05 17:44:51 +0900

    Convert to RESTful

--- 05_Django/03.django.crud/articles/admin.py
+++ 05_Django/03.django.crud/articles/admin.py
@@ -3,10 +3,10 @@ from .models import Article, Comment
 
 # Register your models here.
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'title', 'content', 'created_at', 'updated_at',)
+    list_display = ('pk', 'title', 'content', 'image', 'created_at', 'updated_at',)
 
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'content', 'created_at', 'updated_at',)
+    list_display = ('pk', 'content', 'created_at', 'updated_at', 'article',)
 
 admin.site.register(Article, ArticleAdmin)
 admin.site.register(Comment, CommentAdmin)
--- 05_Django/03.django.crud/articles/views.py
+++ 05_Django/03.django.crud/articles/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Article
+from .models import Article, Comment
 
 # Create your views here. 마지막 글이 맨 위로 오게 뒤집는게 [::-1]
 def index(request):
@@ -26,8 +26,8 @@ def create(request):
     if request.method == 'POST':
         title = request.POST.get('title')
         content = request.POST.get('content')
-
-        article = Article(title=title, content=content)
+        image = request.FILES.get('image')
+        article = Article(title=title, content=content, image=image)
         article.save()
         return redirect('articles:detail', article.pk)
     # GET 요청일 경우 -> 사용자에게 폼 보여주기
@@ -38,7 +38,12 @@ def create(request):
 # 게시글 상세정보를 가져오는 함수
 def detail(request, article_pk):
     article = Article.objects.get(pk=article_pk)
-    context = {'article': article}
+    # article을 참조하고 있는 comment 테이블 값들 가져오기
+    comments = article.comment_set.all()
+    context = {
+        'article': article,
+        'comments': comments,
+    }
     return render(request, 'articles/detail.html', context)
 
 # def detail(request, article_pk):
@@ -87,3 +92,22 @@ def update(request, article_pk):
 
     # .html 파일 내에서 '{% url %} 템플릿 태그' 사용햇을 때(헷갈림 주의!!!!)
     # <a href="{% url articles:detail article.pk %}">
+
+def comments_create(request, article_pk):
+    article = Article.objects.get(pk=article_pk)
+    if request.method == "POST":
+        content = request.POST.get('content')
+        comment = Comment(article=article, content=content)
+        comment.save()
+        return redirect('articles:detail', article.pk)
+    else:
+        return redirect('articles:detail', article.pk)
+
+def comments_delete(request, article_pk, comment_pk):
+    article = Article.objects.get(pk=article_pk)
+    if request.method == "POST":
+        comment = Comment.objects.get(pk=comment_pk)
+        comment.delete()
+        return redirect('articles:detail', article.pk)
+    else:
+        return redirect('articles:detail', article.pk)
